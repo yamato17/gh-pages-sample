6cdbc24998eb9a71f7cfd589dab14fd9c9a7b6fa
yamato17 <yamato1033@gmail.com>
2020-07-30 19:31:16 +0000

    edit

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+    console.log('[ServiceWorker] Install');
 });
 
 self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
+    console.log('[ServiceWorker] Activate');
 });
 
-// 現状では、この処理を書かないとService Workerが有効と判定されないようです
+// サービスワーカー有効化に必須
 self.addEventListener('fetch', function(event) {});
